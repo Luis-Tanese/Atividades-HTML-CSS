360ecb2020787babb9aab4255abab8ec2df82bb6
Luis Tanese <luistanese@gmail.com>
2024-07-08 09:44:09 -0300

    Update script.js

--- ATIVIDADE 3 (QUINTA 27 06)/script.js
+++ ATIVIDADE 3 (QUINTA 27 06)/script.js
@@ -1,13 +1,21 @@
-const form = document.getElementById("registroForm");
-const senha = document.getElementById("senha").value;
-const confirmarSenha = document.getElementById("confirmar").value;
-
-document.addEventListener("DOMContentLoaded", function(){
-    
-    form.addEventListener("submit", function(event){
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("registroForm");
+    const termos = document.getElementById("termos");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const senha = document.getElementById("senha").value;
+        const confirmarSenha = document.getElementById("confirmar").value;
+        let valid = true;
+        if(!termos.checked){
+            alert("Você precisa aceitar os termos de serviço.");
+            valid = false;
+        }
         if(senha !== confirmarSenha){
             alert("As senhas não coincidem. Por favor, verifique e tente novamente.");
-            event.preventDefault();
+            valid = false;
+        }
+        if(valid){
+            window.location.href = "/Sillycate/index1.html";
         }
     });
 });
